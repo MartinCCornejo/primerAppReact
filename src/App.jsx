5d7da6f417129767b2750273fb4a8f802558c464
Martin Cristian Cornejo <martincckd130@gmail.com>
2023-12-12 01:25:41 -0300

    Agregue el maquetado del contador, faltaria hacer la logica

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
+import Contador from "./components/Contador";
 import Informacion from "./components/Informacion";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   // aqui vamos a agregar la logica del componente 
-
   return (
     // aqui va un poquito de logica 
     // luego va el maquetado del componente 
     <section className="container my-4">
       <h1 className="display-3 text-center mb-3">PrimerApp React</h1>
       <Informacion tituloSecundario = 'Este titulo fue creado con un props'/>
+      <Contador/>
     </section>
   )
 }
