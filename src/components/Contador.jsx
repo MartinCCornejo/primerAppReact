cc5cf7ac8832f49afdcfe431c26b21a6c6047eeb
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-01-26 20:47:57 -0300

    Agregamos un boton para decrementar el número

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -6,9 +6,12 @@ const Contador = () => {
   return (
     <article className="text-center mt-4">
       <h3>Contador</h3>
-      <p className="fw-bold">{numero}</p>
-      <button className="btn btn-primary" onClick={() => setNumero(numero + 1)}>
+      <p className="fw-bold fs-3">{numero}</p>
+      <button className="btn btn-primary fw-bold" onClick={() => setNumero(numero + 1)}>
         +1
+      </button>
+      <button className="btn btn-danger ms-2 fw-bold" onClick={() => setNumero(numero - 1)}>
+        -1
       </button>
     </article>
   );
